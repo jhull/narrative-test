b25c35b363e5d54207d2ca2f67734fd28237a2bb
jhull <jim@narrativefirst.com>
2022-06-24 14:10:23 -0700

    switched: for slots

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,15 +1,18 @@
 <template>
+<div>
   <div class="max-w-2xl px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 sm:rounded">
-    <!-- Fetch and display the Markdown document from current path -->
-    <ContentDoc class="prose prose-gray dark:prose-invert max-w-none">
-      <!-- Slot if document is not found -->
-      <template #not-found>
-        <h1 class="text-2xl">
-          Page not found
-        </h1>
-      </template>
+    <ContentDoc v-slot="{ doc }"
+      class="prose prose-gray dark:prose-invert max-w-none">
+      <!-- <BannerImage v-if="doc.banner_slug" :banner_slug="doc.banner_slug" class="mb-8" /> -->
+      <div class="mb-8 prose prose-gray dark:prose-invert max-w-none">
+        <h1>{{ doc.title }}</h1>
+        <p class="-mt-6 text-gray-400">{{ doc.tldr }}</p>
+      </div>
+      <ContentRenderer class="prose prose-gray dark:prose-invert max-w-none" :value="doc" />
     </ContentDoc>
   </div>
+  <!-- <TeamListing v-if="$route.path === '/about'" /> -->
+</div>
 </template>
 
 <style lang="postcss">
